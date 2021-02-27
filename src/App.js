5afb39e1a42dca194e65aeba3f5e35860f0bb9e4
Nikolas C <nrc170001@utdallas.edu>
2021-02-27 16:40:05 -0600

    nodisc2

--- src/App.js
+++ src/App.js
@@ -6,31 +6,6 @@ import Home from "./Home.js";
 
 
 
-window.onload = () => {
-const fragment = new URLSearchParams(window.location.hash.slice(1));
-
-			if (fragment.has("access_token")) {
-				const accessToken = fragment.get("access_token");
-				const tokenType = fragment.get("token_type");
-
-				fetch('https://discord.com/api/users/@me', {
-					headers: {
-						authorization: `${tokenType} ${accessToken}`
-					}
-				})
-					.then(res => res.json())
-					.then(response => {
-						const { username, discriminator } = response;
-						document.getElementById('info').innerText += ` ${username}#${discriminator}`;
-					})
-					.catch(console.error);
-
-			}
-			else {
-				console.log("else");
-			}
-  }
-
 
 function App() {
   return (
